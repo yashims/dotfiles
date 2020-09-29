a2b1cd36a8349965db8cd598d6ce7f3029ce5277
Yashiro Masaya <yashims85@gmail.com>
2020-09-29 18:33:52 +0900

    feat add wsl settings

--- zsh/ssh-agent.sh
+++ zsh/ssh-agent.sh
@@ -1,10 +1,12 @@
 
 # Share ssh-agent between terminals.
 function start-ssh-agent() {
+    SOKET_FILE="$HOME/.ssh/agent.sock"
+
     if [[ -S "$SSH_AUTH_SOCK" ]]; then
         case $SSH_AUTH_SOCK in
         /tmp/*/agent.[0-9]*)
-            ln -fs $SSH_AUTH_SOCK $HOME/.ssh/agent.sock && export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+            ln -fs $SSH_AUTH_SOCK $SOKET_FILE && export SSH_AUTH_SOCK=$SOKET_FILE
             echo 'SSH Agent is forwarded!'
             ;;
         *)
@@ -12,11 +14,11 @@ function start-ssh-agent() {
             ;;
         esac
     elif [[ -S $HOME/.ssh/agent.sock ]]; then
-        export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+        export SSH_AUTH_SOCK=$SOKET_FILE
         echo 'SSH Agent is already started!'
     else
         echo 'Launch SSH Agent'
-        eval `ssh-agent -a $HOME/.ssh/agent.sock`
+        eval `ssh-agent -a $SOKET_FILE`
     fi
 }
 
